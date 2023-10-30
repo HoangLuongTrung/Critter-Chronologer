205415a58983545b1c5f87d5deee45739dec2266
Luong Trung <trunghlbuh@gmail.com>
2023-10-30 17:45:08 +0700

    Add some Repositories and Services

---
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repositories/PetRepository.java
@@ -0,0 +1,7 @@
+package com.udacity.jdnd.course3.critter.repositories;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class PetRepository {
+}
---
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/services/PetService.java
@@ -0,0 +1,8 @@
+package com.udacity.jdnd.course3.critter.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class PetService {
+}
