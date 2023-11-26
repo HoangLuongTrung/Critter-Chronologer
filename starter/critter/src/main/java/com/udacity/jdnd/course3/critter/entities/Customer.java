4849f3b6c8fa7ee50ced93c48f43a6098f5b0163
Luong Trung <trunghlbuh@gmail.com>
2023-11-27 00:57:40 +0700

    Fix review

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/entities/Customer.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/entities/Customer.java
@@ -13,7 +13,7 @@ import java.util.List;
 @Table(name = "CUSTOMER")
 public class Customer {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Nationalized
